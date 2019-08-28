251e19c92d55d6772c126f1e8a0c281cc6c57867
Weiwei Yang <weiwei.yang@cloudera.com>
2019-08-28 15:46:52 +0800

    make sure containers are released when on task failures

--- pkg/shim/scheduler_test.go
+++ pkg/shim/scheduler_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cloudera/yunikorn-k8shim/pkg/common/test"
 	"github.com/cloudera/yunikorn-scheduler-interface/lib/go/si"
 	"go.uber.org/zap"
+	"k8s.io/api/core/v1"
 	"testing"
 	"time"
 )
@@ -136,7 +137,7 @@ partitions:
 	app0001 = cluster.newApplication("app0001", "root.a")
 	cluster.addTask("task0001", taskResource, app0001)
 	cluster.addApplication(app0001)
-	cluster.waitAndAssertApplicationState(t, "app0001", events.States().Application.Accepted)
+	cluster.waitAndAssertApplicationState(t, "app0001", events.States().Application.Running)
 	cluster.waitAndAssertTaskState(t, "app0001", "task0001", events.States().Task.Bound)
 }
 
@@ -156,6 +157,74 @@ func TestSchedulerRegistrationFailed(t *testing.T){
 
 	if err := waitShimSchedulerState(shim, events.States().Scheduler.Stopped, 5 * time.Second); err !=nil {
 		t.Fatalf("%v", err)
+	}
+}
+
+func TestTaskFailures(t *testing.T) {
+	configData := `
+partitions:
+  -
+    name: default
+    queues:
+      -
+        name: root
+        queues:
+          -
+            name: a
+            resources:
+              guaranteed:
+                memory: 100
+                vcore: 10
+              max:
+                memory: 100
+                vcore: 10
+`
+	// init and register scheduler
+	cluster := MockScheduler{}
+	// mock pod bind failures
+	cluster.bindFn = func(pod *v1.Pod, hostId string) error {
+		if pod.Name == "task0001" {
+			return fmt.Errorf("mocked error when binding the pod")
+		}
+		return nil
+	}
+	cluster.init(configData)
+	cluster.start()
+	defer cluster.stop()
+
+	// ensure scheduler state
+	cluster.waitForSchedulerState(t, events.States().Scheduler.Running)
+
+	// register nodes
+	if err := cluster.addNode("test.host.01", 100, 10); err != nil {
+		t.Fatalf("add node failed %v", err)
+	}
+	if err := cluster.addNode("test.host.02", 100, 10); err != nil {
+		t.Fatalf("add node failed %v", err)
+	}
+
+	// create app and tasks
+	app0001 := cluster.newApplication("app0001", "root.a")
+	taskResource := common.NewResourceBuilder().
+		AddResource(common.Memory, 50).
+		AddResource(common.CPU, 5).
+		Build()
+	cluster.addTask("task0001", taskResource, app0001)
+	cluster.addTask("task0002", taskResource, app0001)
+
+	// add app to context
+	cluster.addApplication(app0001)
+
+	// wait for scheduling app and tasks
+	// verify app state
+	cluster.waitAndAssertApplicationState(t, "app0001", events.States().Application.Running)
+	cluster.waitAndAssertTaskState(t, "app0001", "task0001", events.States().Task.Failed)
+	cluster.waitAndAssertTaskState(t, "app0001", "task0002", events.States().Task.Bound)
+
+	// one task get bound, one ask failed, so we are expecting only 1 allocation in the scheduler
+	if err := cluster.waitAndVerifySchedulerAllocations("root.a",
+		"[test-cluster]default","app0001", 1); err != nil {
+		t.Fatalf("number of allocations is not expected %v", err)
 	}
 }
 
