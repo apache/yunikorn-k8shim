147e413e1efad218f8006606e1c0a2c286dfbe05
Weiwei Yang <wyang@cloudera.com>
2020-02-14 22:17:53 -0800

    This PR fixes following issues  1. when there is no nodes in the cluster, recovery timesout  2. support KUBECONFIG environment variable for local mode

--- pkg/cache/context_recovery.go
+++ pkg/cache/context_recovery.go
@@ -205,12 +205,11 @@ func waitAndListPods(lister v1.PodLister) ([]*corev1.Pod, error) {
 
 func waitAndListNodes(lister v1.NodeLister) ([]*corev1.Node, error) {
 	var allNodes []*corev1.Node
+	var listErr error
 	err := utils.WaitForCondition(func() bool {
 		//nolint:errcheck
-		if allNodes, _ = lister.List(labels.Everything()); len(allNodes) > 0 {
-			return true
-		}
-		return false
+		allNodes, listErr = lister.List(labels.Everything())
+		return listErr == nil
 	}, time.Second, time.Minute)
 	if err != nil {
 		return nil, err
