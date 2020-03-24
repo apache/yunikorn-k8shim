2e66bd5d53fc7c98cacdc6a6d69f198a4d5dd6bb
Weiwei Yang <wyang@cloudera.com>
2020-03-24 12:28:59 -0700

    [YUNIKORN-45] Task is not properly released when they are removed from pending state.

---
+++ pkg/cache/task_test.go
@@ -0,0 +1,302 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package cache
+
+import (
+	"testing"
+
+	"github.com/apache/incubator-yunikorn-k8shim/pkg/client"
+	"github.com/apache/incubator-yunikorn-k8shim/pkg/common/events"
+	"github.com/apache/incubator-yunikorn-scheduler-interface/lib/go/si"
+	"gotest.tools/assert"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	apis "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestTaskStateTransitions(t *testing.T) {
+	mockedSchedulerApi := newMockSchedulerAPI()
+	mockedContext := initContextForTest()
+	resources := make(map[v1.ResourceName]resource.Quantity)
+	containers := make([]v1.Container, 0)
+	containers = append(containers, v1.Container{
+		Name: "container-01",
+		Resources: v1.ResourceRequirements{
+			Requests: resources,
+		},
+	})
+	pod := &v1.Pod{
+		TypeMeta: apis.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: apis.ObjectMeta{
+			Name: "pod-resource-test-00001",
+			UID:  "UID-00001",
+		},
+		Spec: v1.PodSpec{
+			Containers: containers,
+		},
+	}
+
+	app := NewApplication("app01", "root.default",
+		"bob", map[string]string{}, mockedSchedulerApi)
+	task := NewTask("task01", app, mockedContext, pod)
+	assert.Equal(t, task.GetTaskState(), events.States().Task.New)
+
+	// new task
+	event0 := NewSimpleTaskEvent(task.applicationID, task.taskID, events.InitTask)
+	if err := task.handle(event0); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Pending)
+
+	// submit task to the scheduler-core
+	event1 := NewSubmitTaskEvent(app.applicationID, task.taskID)
+	if err := task.handle(event1); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Scheduling)
+
+	// allocated
+	event2 := NewAllocateTaskEvent(app.applicationID, task.taskID, string(pod.UID), "node-1")
+	if err := task.handle(event2); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Allocated)
+
+	// bound
+	event3 := NewBindTaskEvent(app.applicationID, task.taskID)
+	if err := task.handle(event3); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Bound)
+
+	// complete
+	event4 := NewSimpleTaskEvent(app.applicationID, task.taskID, events.CompleteTask)
+	if err := task.handle(event4); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Completed)
+}
+
+func TestTaskIllegalEventHandling(t *testing.T) {
+	mockedSchedulerApi := newMockSchedulerAPI()
+	mockedContext := initContextForTest()
+	resources := make(map[v1.ResourceName]resource.Quantity)
+	containers := make([]v1.Container, 0)
+	containers = append(containers, v1.Container{
+		Name: "container-01",
+		Resources: v1.ResourceRequirements{
+			Requests: resources,
+		},
+	})
+	pod := &v1.Pod{
+		TypeMeta: apis.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: apis.ObjectMeta{
+			Name: "pod-resource-test-00001",
+			UID:  "UID-00001",
+		},
+		Spec: v1.PodSpec{
+			Containers: containers,
+		},
+	}
+
+	app := NewApplication("app01", "root.default",
+		"bob", map[string]string{}, mockedSchedulerApi)
+	task := NewTask("task01", app, mockedContext, pod)
+	assert.Equal(t, task.GetTaskState(), events.States().Task.New)
+
+	// new task
+	event0 := NewSimpleTaskEvent(task.applicationID, task.taskID, events.InitTask)
+	if err := task.handle(event0); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Pending)
+
+	// verify illegal event handling logic
+	event2 := NewAllocateTaskEvent(app.applicationID, task.taskID, string(pod.UID), "node-1")
+	err := task.handle(event2)
+	if err == nil {
+		t.Fatal("expecting an error, event AllocateTask is illegal when task is Pending")
+	}
+	// task state should not have changed
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Pending)
+}
+
+func TestReleaseTaskAllocation(t *testing.T) {
+	mockedSchedulerApi := newMockSchedulerAPI()
+	mockedContext := initContextForTest()
+	apiProvider := mockedContext.apiProvider
+	mockedApiProvider, ok := apiProvider.(*client.MockedAPIProvider)
+	if !ok {
+		t.Fatal("expecting MockedAPIProvider")
+	}
+
+	resources := make(map[v1.ResourceName]resource.Quantity)
+	containers := make([]v1.Container, 0)
+	containers = append(containers, v1.Container{
+		Name: "container-01",
+		Resources: v1.ResourceRequirements{
+			Requests: resources,
+		},
+	})
+	pod := &v1.Pod{
+		TypeMeta: apis.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: apis.ObjectMeta{
+			Name: "pod-resource-test-00001",
+			UID:  "UID-00001",
+		},
+		Spec: v1.PodSpec{
+			Containers: containers,
+		},
+	}
+
+	app := NewApplication("app01", "root.default",
+		"bob", map[string]string{}, mockedSchedulerApi)
+	task := NewTask("task01", app, mockedContext, pod)
+	assert.Equal(t, task.GetTaskState(), events.States().Task.New)
+
+	// new task
+	event0 := NewSimpleTaskEvent(task.applicationID, task.taskID, events.InitTask)
+	if err := task.handle(event0); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Pending)
+
+	// submit task to the scheduler-core
+	event1 := NewSubmitTaskEvent(app.applicationID, task.taskID)
+	if err := task.handle(event1); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Scheduling)
+
+	// allocated
+	event2 := NewAllocateTaskEvent(app.applicationID, task.taskID, string(pod.UID), "node-1")
+	if err := task.handle(event2); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Allocated)
+
+	// bound
+	event3 := NewBindTaskEvent(app.applicationID, task.taskID)
+	if err := task.handle(event3); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Bound)
+
+	// the mocked update function does nothing than verify the coming messages
+	// this is to verify we are sending correct info to the scheduler core
+	mockedApiProvider.MockSchedulerApiUpdateFn(func(request *si.UpdateRequest) error {
+		assert.Assert(t, request.Releases != nil)
+		assert.Assert(t, request.Releases.AllocationsToRelease != nil)
+		assert.Equal(t, request.Releases.AllocationsToRelease[0].ApplicationID, app.applicationID)
+		assert.Equal(t, request.Releases.AllocationsToRelease[0].PartitionName, "default")
+		assert.Equal(t, request.Releases.AllocationsToRelease[0].UUID, "UID-00001")
+		return nil
+	})
+
+	// complete
+	event4 := NewSimpleTaskEvent(app.applicationID, task.taskID, events.CompleteTask)
+	if err := task.handle(event4); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Completed)
+	// 2 updates call, 1 for submit, 1 for release
+	assert.Equal(t, mockedApiProvider.GetSchedulerApiUpdateCount(), int32(2))
+}
+
+func TestReleaseTaskAsk(t *testing.T) {
+	mockedSchedulerApi := newMockSchedulerAPI()
+	mockedContext := initContextForTest()
+	apiProvider := mockedContext.apiProvider
+	mockedApiProvider, ok := apiProvider.(*client.MockedAPIProvider)
+	if !ok {
+		t.Fatal("expecting MockedAPIProvider")
+	}
+
+	resources := make(map[v1.ResourceName]resource.Quantity)
+	containers := make([]v1.Container, 0)
+	containers = append(containers, v1.Container{
+		Name: "container-01",
+		Resources: v1.ResourceRequirements{
+			Requests: resources,
+		},
+	})
+	pod := &v1.Pod{
+		TypeMeta: apis.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: apis.ObjectMeta{
+			Name: "pod-resource-test-00001",
+			UID:  "UID-00001",
+		},
+		Spec: v1.PodSpec{
+			Containers: containers,
+		},
+	}
+
+	app := NewApplication("app01", "root.default",
+		"bob", map[string]string{}, mockedSchedulerApi)
+	task := NewTask("task01", app, mockedContext, pod)
+	assert.Equal(t, task.GetTaskState(), events.States().Task.New)
+
+	// new task
+	event0 := NewSimpleTaskEvent(task.applicationID, task.taskID, events.InitTask)
+	if err := task.handle(event0); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Pending)
+
+	// submit task to the scheduler-core
+	// the task will be at scheduling state from this point on
+	event1 := NewSubmitTaskEvent(app.applicationID, task.taskID)
+	if err := task.handle(event1); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Scheduling)
+
+	// the mocked update function does nothing than verify the coming messages
+	// this is to verify we are sending correct info to the scheduler core
+	mockedApiProvider.MockSchedulerApiUpdateFn(func(request *si.UpdateRequest) error {
+		assert.Assert(t, request.Releases != nil)
+		assert.Assert(t, request.Releases.AllocationsToRelease == nil)
+		assert.Assert(t, request.Releases.AllocationAsksToRelease != nil)
+		assert.Equal(t, request.Releases.AllocationAsksToRelease[0].ApplicationID, app.applicationID)
+		assert.Equal(t, request.Releases.AllocationAsksToRelease[0].PartitionName, "default")
+		assert.Equal(t, request.Releases.AllocationAsksToRelease[0].Allocationkey, task.taskID)
+		return nil
+	})
+
+	// complete
+	event4 := NewSimpleTaskEvent(app.applicationID, task.taskID, events.CompleteTask)
+	if err := task.handle(event4); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, task.GetTaskState(), events.States().Task.Completed)
+	// 2 updates call, 1 for submit, 1 for release
+	assert.Equal(t, mockedApiProvider.GetSchedulerApiUpdateCount(), int32(2))
+}
