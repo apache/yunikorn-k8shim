3614259a1d6786d2ec2044a387c9a936fbc98ea5
Ting-Yao Huang <61864060+HuangTing-Yao@users.noreply.github.com>
2020-06-08 13:55:25 +0800

    Update update-codegen.sh

--- pkg/apis/yunikorn.apache.org/update-codegen.sh
+++ pkg/apis/yunikorn.apache.org/update-codegen.sh
@@ -20,7 +20,7 @@
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-GO111MODULE=off
+
 bash k8s.io/code-generator/generate-groups.sh "all" \
   github.com/apache/incubator-yunikorn-k8shim/pkg/client github.com/apache/incubator-yunikorn-k8shim/pkg/apis \
   "yunikorn.apache.org:v1alpha1" \
