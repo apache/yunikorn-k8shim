641ff525cd92e9687a0c380e208634d752b99a0a
Ayub Khan Pathan <apathan@cloudera.com>
2020-06-09 18:05:52 -0700

    [YUNIKORN-163] Add initial e2e testing framework
    This PR adds the initial e2e testing framework code in k8shim, these tests
    are triggered upon each PR. It firstly installs a K8s cluster with KIND, then
    deploy yunikorn with helm charts, then run a bunch of tests to verify yunikorn
    functionalities.
    
    Fixes #130.

---
+++ test/e2e/framework/helpers/deserializer.go
@@ -0,0 +1,45 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package helpers
+
+import (
+	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+func yaml2Obj(yamlPath string) (runtime.Object, error) {
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	content, err := GetFileContents(yamlPath)
+	if err == nil {
+		obj, _, err1 := decode(content, nil, nil)
+		return obj, err1
+	}
+	return nil, err
+}
+
+func Y2Map(yamlPath string) (map[interface{}]interface{}, error) {
+	m := make(map[interface{}]interface{})
+	content, err := GetFileContents(yamlPath)
+	if err == nil {
+		err1 := yaml.Unmarshal(content, &m)
+		return m, err1
+	}
+	return nil, err
+}
