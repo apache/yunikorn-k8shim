ced5db8c6305ba4992eda13f1f6b7e30acd971b8
apathan <apathan@cloudera.com>
2020-08-03 19:01:05 -0700

    fixing goimport issue

--- test/e2e/app/app_test.go
+++ test/e2e/app/app_test.go
@@ -18,10 +18,11 @@
 package app
 
 import (
-	"github.com/apache/incubator-yunikorn-k8shim/test/e2e/framework/helpers/yunikorn"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
+
+	"github.com/apache/incubator-yunikorn-k8shim/test/e2e/framework/helpers/yunikorn"
 
 	"github.com/apache/incubator-yunikorn-k8shim/pkg/apis/yunikorn.apache.org/v1alpha1"
 	crdclientset "github.com/apache/incubator-yunikorn-k8shim/pkg/client/clientset/versioned"
