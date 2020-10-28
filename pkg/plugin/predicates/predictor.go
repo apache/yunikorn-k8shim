55b183b13f44bdb654d00060c55a99f2529b2f2a
kmarton <kmarton@cloudera.com>
2020-10-28 20:50:17 +0100

    Added missing predicate

--- pkg/plugin/predicates/predictor.go
+++ pkg/plugin/predicates/predictor.go
@@ -51,8 +51,9 @@ var defaultSchedulerPolicy = schedulerapi.Policy{
 		// If replacing the default scheduler you must have the volume predicate included:
 		// https://docs.okd.io/latest/admin_guide/scheduling/scheduler.html#static-predicates
 		{Name: predicates.CheckVolumeBindingPred},
-		//PodFitsResourcesPred, PodFitsHostPortsPred, MatchNodeSelectorPred forms the GeneralPredicate
+		//PodFitsResourcesPred, PodFitsHostPortsPred, MatchNodeSelectorPred and HostNamePred forms the GeneralPredicate
 		{Name: predicates.PodFitsResourcesPred},
+		{Name: predicates.HostNamePred},
 		{Name: predicates.PodFitsHostPortsPred},
 		{Name: predicates.MatchNodeSelectorPred},
 	},
