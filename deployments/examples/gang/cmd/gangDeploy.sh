3c291903002a94bc365b6d210d3670396566286e
HuangTing-Yao <acs104112@gm.ntcu.edu.tw>
2021-02-15 13:31:39 +1100

    [YUNIKORN-509] Simplify the gang deploy script (#229)
    iSimplify the gang test script, re-use yaml file and cleanup resource on
    failure or timeout.
    
    Fixes: #229

--- deployments/examples/gang/cmd/gangDeploy.sh
+++ deployments/examples/gang/cmd/gangDeploy.sh
@@ -15,57 +15,47 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-#limitations under the License.
+# limitations under the License.
 #
 
-# gangDeploy.sh <job amount> <pod amount> <gang member> <task run time(sec)>
+# gangDeploy.sh <job amount> <gang member> <task run time(sec)>
 set -o errexit
 set -o nounset
 set -o pipefail
 
-JOBAMOUNT=$1
-GANGMEMBER=$2
-RUNTIMESEC=$3
+# check parameter is integer or not
+if ([ "$1" -gt 0 ] && [ "$2" -gt 0 ] && [ "$3" -gt 0 ]) 2>/dev/null
+then
+    JOBAMOUNT=$1
+    GANGMEMBER=$2
+    RUNTIMESEC=$3
+else    
+    echo "ERROR: input parameters must be an integer."
+    exit 1
+fi
 
-# create service
-kubectl create -f <(cat << EOF
-apiVersion: v1
-kind: Service
-metadata:
-  name: gangservice
-  labels:
-    app: gang
-spec:
-  selector:
-    app: gang
-  type: ClusterIP
-  ports:
-  - protocol: TCP
-    port: 8863
-    targetPort: 8863
-EOF) 
-# create job counter web server
-kubectl create -f <(cat << EOF
-apiVersion: v1
-kind: Pod
-metadata:
-  name: gangweb
-  labels:
-    app: gang
-    queue: root.sandbox
-spec:
-  schedulerName: yunikorn
-  containers:
-    - name: gangweb
-      image: apache/yunikorn:simulation-gang-coordinator-latest
-      imagePullPolicy: Never
-      ports:
-        - containerPort: 8863
-EOF)
+TIMEOUT=0
+
+# create service and job counter web server
+WORKDIR=$(cd "$(dirname "$0")/../"; pwd)
+if [[ -f $WORKDIR/gang-coordinator.yaml ]]; then
+  kubectl apply -f $WORKDIR/gang-coordinator.yaml
+else
+  echo "ERROR: gang-coordinator.yaml is not found in path $WORKDIR"
+  exit
+fi
+
 # wait for web server to be running
-until grep 'Running' <(kubectl get pod gangweb -o=jsonpath='{.status.phase}'); do
+until grep -q 'Running' <(kubectl get pod gangweb -o=jsonpath='{.status.phase}'); do
   sleep 1
+  TIMEOUT=$(($TIMEOUT+1))
+  if [ $TIMEOUT -ge 20 ]; then
+    echo "ERROR: Timeout for waiting web server"
+    kubectl delete -f $WORKDIR/gang-coordinator.yaml
+    exit
+  fi
 done
+
 # create gang jobs
 for i in $(seq "$JOBAMOUNT"); do
   kubectl create -f <(cat << EOF
@@ -96,5 +86,6 @@ spec:
           value: "$RUNTIMESEC"
       restartPolicy: Never
       schedulerName: yunikorn
-EOF)
+EOF
+)
 done
