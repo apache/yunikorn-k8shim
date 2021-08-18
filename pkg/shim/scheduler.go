60d239c8757e071d0c5efea2687533e10d060820
Chenya Zhang <chenya_zhang@apple.com>
2021-08-17 17:03:19 -0700

    [YUNIKORN-703]Reduce scheduler recovery timeout and stop failed container (#3)
    Co-authored-by: chenya-zhang <83974445+chenya-zhang@users.noreply.github.com>

--- pkg/shim/scheduler.go
+++ pkg/shim/scheduler.go
@@ -19,6 +19,7 @@
 package main
 
 import (
+	"os"
 	"sync"
 	"time"
 
@@ -145,6 +146,7 @@ func (ss *KubernetesShim) register(e *fsm.Event) {
 
 func (ss *KubernetesShim) handleSchedulerFailure(e *fsm.Event) {
 	ss.stop()
+	os.Exit(1)
 }
 
 func (ss *KubernetesShim) triggerSchedulerStateRecovery(e *fsm.Event) {
@@ -162,7 +164,7 @@ func (ss *KubernetesShim) recoverSchedulerState(e *fsm.Event) {
 		// this step, we collect all the existing allocated pods from api-server,
 		// identify the scheduling identity (aka applicationInfo) from the pod,
 		// and then add these applications to the scheduler.
-		if err := ss.appManager.WaitForRecovery(3 * time.Minute); err != nil {
+		if err := ss.appManager.WaitForRecovery(30 * time.Second); err != nil {
 			// failed
 			log.Logger().Error("scheduler recovery failed", zap.Error(err))
 			dispatcher.Dispatch(ShimSchedulerEvent{
@@ -181,7 +183,7 @@ func (ss *KubernetesShim) recoverSchedulerState(e *fsm.Event) {
 				recoverableAppManagers = append(recoverableAppManagers, m)
 			}
 		}
-		if err := ss.context.WaitForRecovery(recoverableAppManagers, 3*time.Minute); err != nil {
+		if err := ss.context.WaitForRecovery(recoverableAppManagers, 30*time.Second); err != nil {
 			// failed
 			log.Logger().Error("scheduler recovery failed", zap.Error(err))
 			dispatcher.Dispatch(ShimSchedulerEvent{
