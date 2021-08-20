ae5ac7ac68d98614c4b47067b03e38cb72f6f2f0
chenya-zhang <83974445+chenya-zhang@users.noreply.github.com>
2021-08-20 11:56:49 -0700

    [YUNIKORN-703] Reduce scheduler recovery timeout and terminate failed container (#294)
    Reduce the timeout for apps/nodes recovery from 3 minutes to 30 seconds. There is no need to wait for
    minutes for recovery, we want the scheduler recovery can fail faster, so the container can be stopped and
    trigger an auto-restart for failure recovery.
    
    Co-authored-by: Weiwei Yang <wwei@apache.org>

--- pkg/shim/scheduler.go
+++ pkg/shim/scheduler.go
@@ -167,7 +167,7 @@ func (ss *KubernetesShim) recoverSchedulerState(e *fsm.Event) {
 		// this step, we collect all the existing allocated pods from api-server,
 		// identify the scheduling identity (aka applicationInfo) from the pod,
 		// and then add these applications to the scheduler.
-		if err := ss.appManager.WaitForRecovery(3 * time.Minute); err != nil {
+		if err := ss.appManager.WaitForRecovery(30 * time.Second); err != nil {
 			// failed
 			log.Logger().Error("scheduler recovery failed", zap.Error(err))
 			dispatcher.Dispatch(ShimSchedulerEvent{
@@ -186,7 +186,7 @@ func (ss *KubernetesShim) recoverSchedulerState(e *fsm.Event) {
 				recoverableAppManagers = append(recoverableAppManagers, m)
 			}
 		}
-		if err := ss.context.WaitForRecovery(recoverableAppManagers, 3*time.Minute); err != nil {
+		if err := ss.context.WaitForRecovery(recoverableAppManagers, 30*time.Second); err != nil {
 			// failed
 			log.Logger().Error("scheduler recovery failed", zap.Error(err))
 			dispatcher.Dispatch(ShimSchedulerEvent{
