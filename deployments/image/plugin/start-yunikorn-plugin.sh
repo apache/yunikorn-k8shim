753b77fc9ae699b686c00d52d8901b321f9f2741
Peter Bacsko <pbacsko@cloudera.com>
2022-03-29 17:00:26 +0200

    Addressing code review comments

--- deployments/image/plugin/start-yunikorn-plugin.sh
+++ deployments/image/plugin/start-yunikorn-plugin.sh
@@ -17,28 +17,24 @@
 # limitations under the License.
 
 cd /opt/yunikorn/work
-
-[ -n "$CLUSTER_ID" ] && ARGS="$ARGS --yk-cluster-id=$CLUSTER_ID"
-[ -n "$CLUSTER_VERSION" ] && ARGS="$ARGS --yk-cluster-version=$CLUSTER_VERSION"
-[ -n "$POLICY_GROUP" ] && ARGS="$ARGS --yk-policy-group=$POLICY_GROUP"
-[ -n "$SCHEDULING_INTERVAL" ] && ARGS="$ARGS --yk-scheduling-interval=$SCHEDULING_INTERVAL"
-[ -n "$LOG_LEVEL" ] && ARGS="$ARGS --yk-log-level=$LOG_LEVEL"
-[ -n "$LOG_ENCODING" ] && ARGS="$ARGS --yk-log-encoding=$LOG_ENCODING"
-[ -n "$EVENT_CHANNEL_CAPACITY" ] && ARGS="$ARGS --yk-event-channel-capacity=$EVENT_CHANNEL_CAPACITY"
-[ -n "$DISPATCHER_TIMEOUT" ] && ARGS="$ARGS --yk-dispatcher-timeout=$DISPATCHER_TIMEOUT"
-[ -n "$KUBE_CLIENT_QPS" ] && ARGS="$ARGS --yk-kube-qps=$KUBE_CLIENT_QPS"
-[ -n "$KUBE_CLIENT_BURST" ] && ARGS="$ARGS --yk-kube-burst=$KUBE_CLIENT_BURST"
-[ -n "$ENABLE_CONFIG_HOT_REFRESH" ] && ARGS="$ARGS --yk-enable-config-hot-refresh=$ENABLE_CONFIG_HOT_REFRESH"
-[ -n "$DISABLE_GANG_SCHEDULING" ] && ARGS="$ARGS --yk-disable-gang-scheduling=$DISABLE_GANG_SCHEDULING"
-[ -n "$USER_LABEL_KEY" ] && ARGS="$ARGS --yk-user-label-key=$USER_LABEL_KEY"
-[ -n "$PLACEHOLDER_IMAGE" ] && ARGS="$ARGS --yk-placeholder-image=$PLACEHOLDER_IMAGE"
-
-echo "Arguments passed to Yunikorn scheduler plugin: $ARGS"
-
 exec /opt/yunikorn/bin/kube-scheduler \
   --address="${ADDRESS}" \
   --leader-elect="${LEADER_ELECT}" \
   --config="${SCHEDULER_CONFIG}" \
   -v="${VERBOSITY}" \
   --scheduler-name="${SCHEDULER_NAME}" \
-  --yk-scheduler-name="${SCHEDULER_NAME}" $ARGS
+  --yk-cluster-id="${CLUSTER_ID}" \
+  --yk-cluster-version="${CLUSTER_VERSION}" \
+  --yk-policy-group="${POLICY_GROUP}" \
+  --yk-scheduling-interval="${SCHEDULING_INTERVAL}" \
+  --yk-log-level="${LOG_LEVEL}" \
+  --yk-log-encoding="${LOG_ENCODING}" \
+  --yk-event-channel-capacity="${EVENT_CHANNEL_CAPACITY}" \
+  --yk-dispatcher-timeout="${DISPATCHER_TIMEOUT}" \
+  --yk-kube-qps="${KUBE_CLIENT_QPS}" \
+  --yk-kube-burst="${KUBE_CLIENT_BURST}" \
+  --yk-enable-config-hot-refresh="${ENABLE_CONFIG_HOT_REFRESH}" \
+  --yk-disable-gang-scheduling="${DISABLE_GANG_SCHEDULING}" \
+  --yk-user-label-key="${USER_LABEL_KEY}" \
+  --yk-scheduler-name="${SCHEDULER_NAME}" \
+  --yk-placeholder-image="${PLACEHOLDER_IMAGE}"
