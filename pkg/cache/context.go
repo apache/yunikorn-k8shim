7035d27d48d4d47a40ab85863e3582d4b9f99ab0
Manikandan R <manirajv06@gmail.com>
2022-06-09 12:24:05 +0530

    Addressed review comments * Updated depedency version

--- pkg/cache/context.go
+++ pkg/cache/context.go
@@ -693,15 +693,16 @@ func (ctx *Context) AddTask(request *interfaces.AddTaskRequest) interfaces.Manag
 			existingTask, err := app.GetTask(request.Metadata.TaskID)
 			if err != nil {
 				var originator bool
-				var ownerReferenceUID string
+
 				// Is this task the originator of the application?
 				// If yes, then make it as "first pod/owner/driver" of the application and set the task as originator
-				for _, ownerReference := range app.getPlaceholderOwnerReferences() {
-					referenceID := string(ownerReference.UID)
-					if request.Metadata.TaskID == referenceID {
-						originator = true
-						ownerReferenceUID = referenceID
-						break
+				if app.GetOriginatingTask() == nil {
+					for _, ownerReference := range app.getPlaceholderOwnerReferences() {
+						referenceID := string(ownerReference.UID)
+						if request.Metadata.TaskID == referenceID {
+							originator = true
+							break
+						}
 					}
 				}
 				task := NewFromTaskMeta(request.Metadata.TaskID, app, ctx, request.Metadata, originator)
@@ -710,13 +711,15 @@ func (ctx *Context) AddTask(request *interfaces.AddTaskRequest) interfaces.Manag
 					zap.String("appID", app.applicationID),
 					zap.String("taskID", task.taskID),
 					zap.String("taskState", task.GetTaskState()))
-				if app.GetOriginatingTask() == nil {
-					if task, taskErr := app.GetTask(ownerReferenceUID); task != nil && taskErr == nil {
-						log.Logger().Info("app request originating pod added",
-							zap.String("appID", app.applicationID),
-							zap.String("original task", task.GetTaskID()))
-						app.setOriginatingTask(task)
+				if originator {
+					if app.GetOriginatingTask() != nil {
+						log.Logger().Error("Inconsistent state - found another originator task for an application",
+							zap.String("taskId", task.GetTaskID()))
 					}
+					app.setOriginatingTask(task)
+					log.Logger().Info("app request originating pod added",
+						zap.String("appID", app.applicationID),
+						zap.String("original task", task.GetTaskID()))
 				}
 				return task
 			}
