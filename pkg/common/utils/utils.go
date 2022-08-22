89877fd93ebfb59f2e2ea8865d521dedf6716755
Manikandan R <manirajv06@gmail.com>
2022-08-22 11:06:19 +0530

    Addressed review comments

--- pkg/common/utils/utils.go
+++ pkg/common/utils/utils.go
@@ -122,8 +122,10 @@ func GetNamespaceQuotaFromAnnotation(namespaceObj *v1.Namespace) *si.Resource {
 	// 1. namespace.quota
 	// 2. namespace.max.* (Retaining for backwards compatibility. Need to be removed in next major release)
 	if namespaceQuota != "" {
-		log.Logger().Warn("Using namespace.quota instead of namespace.max.* (deprecated) annotation to set cpu and/or memory for namespace though both are available.",
-			zap.String("namespace", namespaceObj.Name))
+		if cpuQuota != "" || memQuota != "" {
+			log.Logger().Warn("Using namespace.quota instead of namespace.max.* (deprecated) annotation to set cpu and/or memory for namespace though both are available.",
+				zap.String("namespace", namespaceObj.Name))
+		}
 		var namespaceQuotaMap map[string]string
 		json.Unmarshal([]byte(namespaceQuota), &namespaceQuotaMap)
 		return common.GetResource(namespaceQuotaMap)
