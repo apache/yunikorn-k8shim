2cd333af51dca320a28b6ae4c518e2e5bcb33f5c
Yu-Lin Chen <chenyulin0719@apache.org>
2024-04-07 08:47:45 +0000

    Fix lint

--- pkg/common/utils/utils.go
+++ pkg/common/utils/utils.go
@@ -377,7 +377,7 @@ func GetPlaceholderFlagFromPodSpec(pod *v1.Pod) bool {
 	if value := GetPodAnnotationValue(pod, constants.OldAnnotationPlaceholderFlag); value != "" { // nolint:staticcheck
 		if v, err := strconv.ParseBool(value); err == nil {
 			log.Log(log.ShimUtils).Warn("Using deprecated placeholder annotation. The support for old placeholder flag will be removed in version 1.7.0",
-				zap.String("annotation", constants.OldAnnotationPlaceholderFlag),
+				zap.String("annotation", constants.OldAnnotationPlaceholderFlag), // nolint:staticcheck
 				zap.String("value", value))
 			return v
 		}
@@ -387,7 +387,7 @@ func GetPlaceholderFlagFromPodSpec(pod *v1.Pod) bool {
 	if value := GetPodLabelValue(pod, constants.OldLabelPlaceholderFlag); value != "" { // nolint:staticcheck
 		if v, err := strconv.ParseBool(value); err == nil {
 			log.Log(log.ShimUtils).Warn("Using deprecated placeholder label. The support for old placeholder flag will be removed in version 1.7.0",
-				zap.String("label", constants.OldLabelPlaceholderFlag),
+				zap.String("label", constants.OldLabelPlaceholderFlag), // nolint:staticcheck
 				zap.String("value", value))
 			return v
 		}
