adb50226ce782999d6e838026980e5e98ad1ccfa
ryan <ryan@ryankert.cc>
2024-05-09 01:58:38 +0800

    follow_up

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -166,7 +166,7 @@ Examples:
   ${NAME} -a test -n yk8s -v kindest/node:v1.29.2
 
   Use a local helm chart path:
-    ${NAME} -a test -n yk8s -v kindest/node:v1.29.0 -p ../yunikorn-release/helm-charts/yunikorn
+    ${NAME} -a test -n yk8s -v kindest/node:v1.29.2 -p ../yunikorn-release/helm-charts/yunikorn
 EOF
 }
 
