a4783cae230c9ed7e64c79a1ca9b23002d999b50
Manikandan R <manirajv06@gmail.com>
2026-07-27 21:16:25 +0530

    Addressed review comments

--- pkg/plugin/predicates/predicate_manager.go
+++ pkg/plugin/predicates/predicate_manager.go
@@ -197,7 +197,7 @@ func (p *predicateManagerImpl) runPreFilterPlugins(ctx context.Context, cycleSta
 			zap.Error(err))
 		return fwk.AsStatus(err), feasibleNodes
 	}
-	var mergedPreFilterResults *fwk.PreFilterResult
+	mergedPreFilterResults := &fwk.PreFilterResult{}
 	for _, pl := range plugins {
 		plugin := pl.Name()
 		nodes, status := pl.PreFilter(ctx, cycleState, pod, allNodes)
@@ -214,10 +214,7 @@ func (p *predicateManagerImpl) runPreFilterPlugins(ctx context.Context, cycleSta
 				zap.Error(err))
 			return fwk.AsStatus(errors.Join(fmt.Errorf("running PreFilter plugin %q: ", plugin), err)), map[string]*si.Empty{}
 		} else {
-			if mergedPreFilterResults == nil {
-				mergedPreFilterResults = &fwk.PreFilterResult{}
-			}
-			mergedPreFilterResults.Merge(nodes)
+			mergedPreFilterResults = mergedPreFilterResults.Merge(nodes)
 		}
 	}
 	if mergedPreFilterResults != nil {
